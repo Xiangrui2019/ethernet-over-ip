ad2498a0c92c939d3f80e278ad6fe80c24a9083f
Xiangrui2019 <xiangrui@aiursoft.com>
2020-06-21 18:36:42 +0800

    support windows

--- ethernet/ethernet.go
+++ ethernet/ethernet.go
@@ -2,6 +2,7 @@ package ethernet
 
 import (
 	"log"
+	"runtime"
 
 	"github.com/songgao/water"
 )
@@ -11,14 +12,25 @@ type Ethernet struct {
 }
 
 func NewEthernet(tap_interface_name string) *Ethernet {
-	tap_interface, err := water.New(water.Config{
-		DeviceType: water.TAP,
-		PlatformSpecificParams: water.PlatformSpecificParams{
-			Name:       tap_interface_name,
-			Persist:    true,
-			MultiQueue: true,
-		},
-	})
+	var err error
+	var tap_interface *water.Interface
+
+	if runtime.GOOS == "linux" {
+		tap_interface, err = water.New(water.Config{
+			DeviceType: water.TAP,
+			PlatformSpecificParams: water.PlatformSpecificParams{
+				Name:       tap_interface_name,
+				Persist:    true,
+				MultiQueue: true,
+			},
+		})
+	}
+
+	if runtime.GOOS == "windows" {
+		tap_interface, err = water.New(water.Config{
+			DeviceType: water.TAP,
+		})
+	}
 
 	if err != nil {
 		log.Fatal(err)
