3c93e11b80a7393b39a4daec182d5cf2b78e62bb
Go Go Go 2019 <xiangrui@aiursoft.com>
2020-06-26 18:53:32 +0800

    add server

--- server/main.go
+++ server/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	_ "ethernet-over-ip/conf"
+	"ethernet-over-ip/shared/ethernet"
+	"ethernet-over-ip/shared/transport"
+	"os"
 )
 
 func main() {
+	eth := ethernet.NewEthernet(os.Getenv("ETHERNET_IFACE_NAME"))
+	server := transport.NewServer(os.Getenv("SERVER_ADDR"), eth)
 
+	server.Serve()
 }
